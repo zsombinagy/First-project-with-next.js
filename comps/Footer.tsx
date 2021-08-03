1d774876a1b978da28fecd2ce9603bfb56b7deaf
iamzsombi <zsombinagy.14@gmail.com>
2021-08-03 16:31:29 +0200

    half the articel, and footer

--- comps/Footer.tsx
+++ comps/Footer.tsx
@@ -12,6 +12,40 @@ const Footer = () => {
          <h3>Pegasus</h3>
          <p>We accept no liability for any misuse!</p>
         </div>
+        <ul className={styles.socials}>
+             <li className={styles.png__li}>
+                 <div className={styles.image}>
+                     <a className={styles.link}>
+                         <Image src="/facebook.png" width={35} height={35} />
+                     </a>
+                 </div>
+             </li>
+             <li className={styles.png__li}>
+                 <div className={styles.image}>
+                     <a className={styles.link}>
+                         <Image src="/instagram.png" width={35} height={35} />
+                     </a>
+                 </div>
+             </li>
+
+             <li className={styles.png__li}>
+                 <div className={styles.image}>
+                     <a className={styles.link}>
+                         <Image src="/twitter.png" width={35} height={35} />
+                     </a>
+                 </div>
+             </li>
+             <li className={styles.png__li}>
+                 <div className={styles.image}>
+                     <a className={styles.link}>
+                         <Image src="/youtube.png" width={40} height={40} />
+                     </a>
+                 </div>
+             </li>
+
+
+         </ul>
+
 
      
      <footer className={styles.footer_bottom}>
